bf5b1ca59e4ce2f025f1474c572cf003f17a2c3e
Nicolas Auler <nicolasauler@usp.br>
2023-06-14 15:02:53 -0300

    synced with gentoo

--- config/nvim/lua/nic/plugins.lua
+++ config/nvim/lua/nic/plugins.lua
@@ -1,8 +1,19 @@
 return {
-    { "ellisonleao/gruvbox.nvim", priority = 1000 },
 
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
+
+
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
         -- or                              , branch = '0.1.1',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -13,7 +24,6 @@ return {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
-            -- add any options here
         },
         dependencies = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -22,6 +32,12 @@ return {
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
+        }
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
         }
     },
 
@@ -34,8 +50,8 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate"
     },
-    {'nvim-treesitter/playground'},
-    {'nvim-treesitter/nvim-treesitter-context'},
+    { 'nvim-treesitter/playground' },
+    { 'nvim-treesitter/nvim-treesitter-context' },
 
     "theprimeagen/harpoon",
     "mbbill/undotree",
@@ -46,25 +62,36 @@ return {
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                                       -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-        {'hrsh7th/cmp-buffer'},       -- Optional
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
-        {'rafamadriz/friendly-snippets'}, -- Optional
-    }
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },       -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+            { 'L3MON4D3/LuaSnip' },       -- Required
+            { 'hrsh7th/cmp-buffer' },     -- Optional
+            { 'hrsh7th/cmp-path' },       -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },   -- Optional
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    },
+
+    "github/copilot.vim",
+    "lervag/vimtex",
+
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        init = function() vim.g.mkdp_filetypes = { "markdown " } end,
+        ft = { "markdown " },
+    },
 
 }
